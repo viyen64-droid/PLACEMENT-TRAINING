55536c4845593c56abcd7774db9772d6d33d7f22
Viyen MK <viyen64@gmail.com>
2026-04-15 08:46:53 +0530

    Add Room class with properties and constructor

--- project1/Room.java
+++ project1/Room.java
@@ -1,1 +1,10 @@
-
+class Room{
+    int roomNumber;
+    String type;
+    Boolean isAvail;
+    Room(int roomNumber,String type){
+        this.roomNumber=roomNumber;
+        this.type=type;
+        this.isAvail=true;
+    }
+}
