2a6cf2a6a418b45a3d58add2c23ecd1f551a2aad
Viyen MK <viyen64@gmail.com>
2026-04-15 10:56:14 +0530

    Add PartTimeEmployee class with salary calculation

---
+++ project2/PartTimeEmployee.java
@@ -0,0 +1,15 @@
+public class PartTimeEmployee extends Employee {
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(int id, String name, int hoursWorked, double hourlyRate) {
+        super(id, name);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+}
